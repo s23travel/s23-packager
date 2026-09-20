f59ebf7cb943f5db6f62a21ed787bda662f8fad5
s23travel <291352446+s23travel@users.noreply.github.com>
2026-09-20 21:46:20 +0000

    Supabase backup [skip ci]

--- backups/data.sql
+++ backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict w8NB170yXPe5TcycVVzakW2SgQ0qvQx8cKldzICZVWBnuF3aIa2ILMj6MPuYmhh
+-- \restrict OegaMRjd4699hQTdmbXtZCqcne1zsuq3vtFz9RJGJyZc6CyiPgTYbWhzjazk3sA
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
@@ -349,6 +349,6 @@ SELECT pg_catalog.setval('"auth"."refresh_tokens_id_seq"', 1, false);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict w8NB170yXPe5TcycVVzakW2SgQ0qvQx8cKldzICZVWBnuF3aIa2ILMj6MPuYmhh
+-- \unrestrict OegaMRjd4699hQTdmbXtZCqcne1zsuq3vtFz9RJGJyZc6CyiPgTYbWhzjazk3sA
 
 RESET ALL;
